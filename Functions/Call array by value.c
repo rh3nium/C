dd3f2a2fb45aaf08d9025b91c4c210bff0596e7e
Riya Hal <riyahal.dev@gmail.com>
2025-04-03 13:37:30 +0530

    Add files via upload

---
+++ Functions/Call array by value.c
@@ -0,0 +1,11 @@
+// Call array by value
+
+#include<stdio.h>
+void modify(int *arr) {
+    arr[2] = 98;
+}
+int main() {
+    int nums[] = {1, 2, 3};
+    modify(nums);
+    printf("Modified array: %d, %d, %d\n", nums[0], nums[1], nums[2]); /* Output: 1, 2, 98 */
+}
