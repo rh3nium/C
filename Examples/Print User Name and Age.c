a83377bb7aa0c36aee6fb796e13eb98ff54ad799
Riya H <riyahal.dev@gmail.com>
2025-05-06 13:43:46 +0530

    Update Print User Name and Age.c

--- Examples/Print User Name and Age.c
+++ Examples/Print User Name and Age.c
@@ -5,7 +5,7 @@ int main() {
     char name[100];
     int age;
     printf("Enter your name: \n");
-    scanf("%s", &name);
+    scanf("%s", name);
     printf("Enter your age: \n");
     scanf("%d", &age);
     printf("Name:%s \n", name);
